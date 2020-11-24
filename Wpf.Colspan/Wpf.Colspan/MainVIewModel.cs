61487825a5a7addb8adece25b9435fc2cdfc8fa5
xiangzongyang <29579895@qq.com>
2020-11-24 15:04:22 +0800

    first commit

---
+++ Wpf.Colspan/Wpf.Colspan/MainVIewModel.cs
@@ -0,0 +1,73 @@
+﻿using GalaSoft.MvvmLight;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Caching;
+
+namespace Wpf.Colspan
+{
+    public class MainVIewModel : ViewModelBase
+    {
+
+        private ObservableCollection<MainModel> _dtaaList = new ObservableCollection<MainModel>();
+
+        public ObservableCollection<MainModel> DataList
+        {
+            get => _dtaaList;
+            set
+            {
+                _dtaaList = value;
+                RaisePropertyChanged();
+            }
+        }
+        public void InitUi()
+        {
+            try
+            {
+                var curModel = Readjson();
+                var propertyInfos = typeof(MainModel).GetProperties(BindingFlags.Public | BindingFlags.Instance).ToList();
+                MainModel mainModel = new MainModel();
+                foreach (var link in curModel.links)
+                {
+                    var property = propertyInfos.FirstOrDefault(s => s.Name.Equals(link.name));
+                    if (link.dataList!= null && link.dataList.Count!=0)
+                    {
+                        property.SetValue(mainModel, link.dataList);
+                    }
+                    else
+                    {
+                        property.SetValue(mainModel, link.data);
+                    }
+                }
+                for (int i = 0; i < 4; i++)
+                {
+                    DataList.Add(mainModel);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public static JsonEntity Readjson()
+        {
+            string jsonfile = AppDomain.CurrentDomain.BaseDirectory + "jsconfig.json";
+            using (StreamReader sr = File.OpenText(jsonfile))
+            {
+                string json = sr.ReadToEnd();
+                JsonEntity list = JsonConvert.DeserializeObject<JsonEntity>(json);
+                sr.Close();
+                sr.Dispose();
+                return list;
+            }
+        }
+    }
+}
---
+++ Wpf.Colspan/Wpf.Colspan/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf.Colspan
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainVIewModel _viewModel = new MainVIewModel();
+            _viewModel.InitUi();
+            DataContext = _viewModel;
+        }
+
+        private void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            try
+            {
+                ScrollViewer sv = e.OriginalSource as ScrollViewer;
+                if (sv != null)
+                {
+                    scrView.ScrollToHorizontalOffset(sv.HorizontalOffset);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
