b4bb7ab356b9fe80339d8233f8fec60433d34998
xiangzongyang <29579895@qq.com>
2020-11-24 14:23:10 +0800

    添加项目文件。

---
+++ Wpf.Colspan/JsonEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf.Colspan
+{
+
+
+    public class JsonEntity
+    {
+        public string name { get; set; }
+        public List<Link> links { get; set; }
+    }
+
+    public class Link
+    {
+        public string name { get; set; }
+        public List<Child> dataList { get; set; }
+        public object data { get; set; }
+    }
+
+
+}
---
+++ Wpf.Colspan/MainModel.cs
@@ -0,0 +1,41 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf.Colspan
+{
+    public class MainModel
+    {
+        public IList<Child> NumberNodes { get; set; } = new List<Child>();
+
+        public List<Child> SelectNodes { get; set; } = new List<Child>();
+
+        public List<Child> DateNodes { get; set; }
+
+        public List<Child> VoucherWordNodes { get; set; } = new List<Child>();
+
+        public string SourceType { get; set; }
+
+        public long VoucherMoney { get; set; }
+
+        public string Auditor { get; set; }
+    }
+
+    public class Child : ViewModelBase
+    {
+        public string word { get; set; }
+
+        public bool _isSelect;
+        /// <summary>
+        /// 选中
+        /// </summary>
+        public bool IsSelect
+        {
+            get { return _isSelect; }
+            set { _isSelect = value; RaisePropertyChanged(); }
+        }
+    }
+}
